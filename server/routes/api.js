ebd2e7591cb1c2e76961a6255594e471a562ea75
devindianushika <devindianushika@gmail.com>
2020-05-30 01:49:16 +0530

    modified getuser method

--- server/routes/api.js
+++ server/routes/api.js
@@ -24,9 +24,6 @@ function verifyToken(req,res,next) {
 }
 
 
-
-
-
 router.get('/',(req,res) =>{
     res.send("Api routes")
 })
@@ -154,5 +151,55 @@ router.get('/special',verifyToken, (req,res) =>{
     ]
     res.json(events)
 })
+
+
+router.get("/getuser", (req, res) => {
+    console.log('get req for all users');
+  User.find({})
+  .exec(function(err,Users) {
+      if(err){
+          console.log("error fetching user data");
+      }else{
+        res.json(Users );
+      }
+   
+  
+    });
+   
+  });
+
+  router.put('/update/:id', (req, res, next) => {
+    const user = { _id: req.params.id };
+    User.updateOne(user, {
+            email: req.body.email,
+            password:req.body.password
+    }).then(doc => {
+      if (!doc) {
+        return res.st(404).end();
+      }
+      return res.status(200).json(doc);
+    })
+      .catch(err => next(err));
+  })
+  
+
+  router.delete('/delete/:id', (req, res, next) => {
+    User.deleteOne({ _id: req.params.id }).then(document => {
+      res.json({ status: 200, message: 'Users data deleted Successfully', document: document });
+    })
+      .catch(err => next(err));
+  })
+  
+  router.get("/:id", (req, res, next) => {
+    User.findById(req.params.id).then(documents => {
+      if (documents) {
+        res.status(200).json(documents);
+      } else {
+        res.status(404).json({ message: 'data not found' });
+      }
+    });
+  });
+  
+
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const PORT = 3000;
 const mongoose = require('mongoose');
 
 
+
 const app = express();
 app.use(cors());
 
